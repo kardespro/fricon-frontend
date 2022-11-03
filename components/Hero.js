0d503f82d19dfd82058c6f3d9c415ff9b050f61e
Nego <61694826+kardespro@users.noreply.github.com>
2022-11-03 12:19:38 +0400

    Small Updates Hero Component

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ import { Badge, Grid } from "@nextui-org/react";
 
 export default function Hero(){
   return(
-    <div className="hero min-h-screen bg-base-200 blurple">
+ <div className="hero min-h-screen bg-base-200 blurple">
   <div className="hero-content text-center">
     <div className="max-w-md">
       <h1 className="text-5xl font-bold"><img src="./logo-no-text.png" className="w-16 h-16 max-w-md mx-auto rounded-2xl blurple bg-blue-700 " /> 
