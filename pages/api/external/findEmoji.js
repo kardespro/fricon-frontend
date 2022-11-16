0845ad8135acaa2033dc4877340b473a9c11b17a
Nego <61694826+kardespro@users.noreply.github.com>
2022-11-16 21:37:49 +0400

    Token

--- pages/api/external/findEmoji.js
+++ pages/api/external/findEmoji.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 export default async function handler(req, res) {
   let q = req.query.id;
   if(!q) return res.json({message: "Invalid Parameters Provided"})
-  let d = await axios.get(`https://discordapp.com/api/v9/guilds/1026571370940874942/emojis/${q}`, {headers: { "Authorization": "Bot MTAzNTQ0ODc2NDM4NDY4MTk4NA.GUooKw.XuFIx3fYWwedYegaaO7TXHCMTath1xB20ET6aA"}}).catch(err => res.json({isAvaliable:"f", error_detalis: null}))
+  let d = await axios.get(`https://discordapp.com/api/v9/guilds/1026571370940874942/emojis/${q}`, {headers: { "Authorization": "Bot tokenhere"}}).catch(err => res.json({isAvaliable:"f", error_detalis: null}))
   //if(d.status === "400") return res.json({avaliable: false})
   let data = d.data
  
